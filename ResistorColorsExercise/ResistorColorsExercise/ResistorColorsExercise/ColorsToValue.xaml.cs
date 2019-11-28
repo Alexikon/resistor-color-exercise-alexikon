9504682c165d3fef90fa0dd3681cf6c9e3e735e9
Tischlinger Alex <32167982+Alexikon@users.noreply.github.com>
2019-11-28 19:03:58 +0100

    made extra points

---
+++ ResistorColorsExercise/ResistorColorsExercise/ResistorColorsExercise/ColorsToValue.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace ResistorColorsExercise
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ColorsToValue : ContentPage
+    {
+        public List<string> Colors1 { get; } = new List<string>
+        {
+            "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey", "White"
+        };
+
+        public List<string> Colors2 { get; } = new List<string>
+        {
+            "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey", "White"
+        };
+
+        public List<string> Colors3 { get; } = new List<string>
+        {
+            "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey", "White", "Gold", "Silver"
+        };
+
+        public string SelectedColor1Result;
+        public string SelectedColor1
+        {
+            get => SelectedColor1Result;
+            set
+            {
+                SelectedColor1Result = value;
+                OnPropertyChanged(nameof(SelectedColor1));
+            }
+        }
+
+        public string SelectedColor2Result;
+        public string SelectedColor2
+        {
+            get => SelectedColor2Result;
+            set
+            {
+                SelectedColor2Result = value;
+                OnPropertyChanged(nameof(SelectedColor2));
+            }
+        }
+
+        public string SelectedColor3Result;
+        public string SelectedColor3
+        {
+            get => SelectedColor3Result;
+            set
+            {
+                SelectedColor3Result = value;
+                OnPropertyChanged(nameof(SelectedColor3));
+            }
+        }
+
+        public decimal ResultValue;
+        public decimal Result
+        {
+            get => ResultValue;
+            set
+            {
+                ResultValue = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public ColorsToValue()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        private void CalculateResistor(object sender, EventArgs e)
+        {
+            // Get the index of colors
+            var index1 = Colors1.IndexOf(SelectedColor1);
+            var index2 = Colors2.IndexOf(SelectedColor2);
+            var index3 = Colors3.IndexOf(SelectedColor3);
+
+            // Calculate the result
+            var result = (index1 * 10 + index2) * ((Int64) Math.Pow(10, index3));
+
+            Result = result;
+
+            /* My Solution
+             * decimal result = 0;
+
+            switch (SelectedColor1)
+            {
+                case "Black": result = 0; break;
+                case "Brown": result = 10; break;
+                case "Red": result = 20; break;
+                case "Orange": result = 30; break;
+                case "Yellow": result = 40; break;
+                case "Green": result = 50; break;
+                case "Blue": result = 60; break;
+                case "Purple": result = 70; break;
+                case "Grey": result = 80; break;
+                case "White": result = 90; break;
+                default: result = 0; break;
+            }
+
+            switch (SelectedColor2)
+            {
+                case "Black": result += 0; break;
+                case "Brown": result += 1; break;
+                case "Red": result += 2; break;
+                case "Orange": result += 3; break;
+                case "Yellow": result += 4; break;
+                case "Green": result += 5; break;
+                case "Blue": result += 6; break;
+                case "Purple": result += 7; break;
+                case "Grey": result += 8; break;
+                case "White": result += 9; break;
+                default: result += 0; break;
+            }
+
+            switch (SelectedColor3)
+            {
+                case "Black": result *= 1; break;
+                case "Brown": result *= 10; break;
+                case "Red": result *= 100; break;
+                case "Orange": result *= 1000; break;
+                case "Yellow": result *= 10000; break;
+                case "Green": result *= 100000; break;
+                case "Blue": result *= 1000000; break;
+                case "Purple": result *= 10000000; break;
+                case "Grey": result *= 100000000; break;
+                case "White": result *= 1000000000; break;
+                case "Gold": result /= 10; break;
+                case "Silver": result /= 100; break;
+                default: result += 0; break;
+            }
+
+            Result = result;*/
+        }
+    }
+}
---
+++ ResistorColorsExercise/ResistorColorsExercise/ResistorColorsExercise/ValueToColors.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ResistorColorsExercise
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ValueToColors : ContentPage
+    {
+        public decimal Input { get; set; }
+
+        public string ResultValue;
+        public string Result
+        {
+            get => ResultValue;
+            set
+            {
+                ResultValue = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public string Color1Result;
+        public string Color1
+        {
+            get => Color1Result;
+            set
+            {
+                Color1Result = value;
+                OnPropertyChanged(nameof(Color1));
+            }
+        }
+
+        public string Color2Result;
+        public string Color2
+        {
+            get => Color2Result;
+            set
+            {
+                Color2Result = value;
+                OnPropertyChanged(nameof(Color2));
+            }
+        }
+
+        public string Color3Result;
+        public string Color3
+        {
+            get => Color3Result;
+            set
+            {
+                Color3Result = value;
+                OnPropertyChanged(nameof(Color3));
+            }
+        }
+
+
+        public ValueToColors()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        private void CalculateColors(object sender, EventArgs e)
+        {
+            long fl;
+
+            if(Input % 1 == 0)
+            {
+                fl = (long) Input;
+
+                // could be a for loop
+                int position = 0;
+                while (fl >= 100)
+                {
+                    if(fl % 10 != 0)
+                    {
+                        Result = "Number cannot be converted";
+
+                        return;
+                    }
+                    fl /= 10;
+
+                    position++;
+                }
+
+                switch(position)
+                {
+                    case 0: Color3 = "Black"; break;
+                    case 1: Color3 = "Brown"; break;
+                    case 2: Color3 = "Red"; break;
+                    case 3: Color3 = "Orange"; break;
+                    case 4: Color3 = "Yellow"; break;
+                    case 5: Color3 = "Green"; break;
+                    case 6: Color3 = "Blue"; break;
+                    case 7: Color3 = "Violet"; break;
+                    case 8: Color3 = "Grey"; break;
+                    case 9: Color3 = "White"; break;
+                    default: Result = "Number cannot be converted"; return;
+                }
+            }
+            else if(Input * 10 % 1 == 0)
+            {
+                fl = (int) (Input * 10);
+
+                Color3 = "Gold";
+            }
+            else if(Input * 100 % 1 == 0)
+            {
+                fl = (int) (Input * 100);
+
+                Color3 = "Silver";
+            }
+            else
+            {
+                Result = "Number cannot be converted";
+
+                return;
+            }
+
+            switch (fl % 10)
+            {
+                case 0: Color2 = "Black"; break;
+                case 1: Color2 = "Brown"; break;
+                case 2: Color2 = "Red"; break;
+                case 3: Color2 = "Orange"; break;
+                case 4: Color2 = "Yellow"; break;
+                case 5: Color2 = "Green"; break;
+                case 6: Color2 = "Blue"; break;
+                case 7: Color2 = "Violet"; break;
+                case 8: Color2 = "Grey"; break;
+                case 9: Color2 = "White"; break;
+                default: Result = "Number cannot be converted"; return;
+            }
+
+            switch ((int) (fl / 10))
+            {
+                case 0: Color1 = "Black"; break;
+                case 1: Color1 = "Brown"; break;
+                case 2: Color1 = "Red"; break;
+                case 3: Color1 = "Orange"; break;
+                case 4: Color1 = "Yellow"; break;
+                case 5: Color1 = "Green"; break;
+                case 6: Color1 = "Blue"; break;
+                case 7: Color1 = "Violet"; break;
+                case 8: Color1 = "Grey"; break;
+                case 9: Color1 = "White"; break;
+                default: Result = "Number cannot be converted"; return;
+            }
+
+            Result = $"Color 1: {Color1}\nColor 2: {Color2}\nColor 3: {Color3}";
+        }
+    }
+}
